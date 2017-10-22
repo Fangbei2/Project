16183894ca8a71428c7e50621b9b73b7d6602c85
Bei.F <835016739@qq.com>
2017-10-22 00:18:58 -0700

    test

--- test/test.c
+++ test/test.c
@@ -1,7 +1,21 @@
 #include <stdio.h>
 
+extern void priintftest();
+//extern int a;
+int a = 1;
+
+
 int main(int argc, char** argv)
 {
-	printf("hello world\n");
+	int a = 3;
+	//extern a = 2;
+	printf("a=%d\n", a);
+	a = 2;
+	printftest();
 	return 0;
+}
+
+void printftest()
+{
+	printf("b=%d\n", a);
 }
